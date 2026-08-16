838a6ac87341120128e7b8fc0ae57ca5172d7e5b
shristi nepali <shristinepali010@gmail.com>
2026-08-16 15:56:08 +0600

    remove database debug logs

--- src/db/index.js
+++ src/db/index.js
@@ -4,10 +4,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async() =>{
     try {
-        console.log("MONGODB_URI:", process.env.MONGODB_URI);
-console.log("DB_NAME:", DB_NAME);
-
-     const  connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    const  connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
      console.log(`\n mongoDB connected !! DB HOST ${connectionInstance.connection.host}`);
      
     } catch (error) {
