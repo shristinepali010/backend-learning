e8d81ec8f9a59f0bf9d80c9593ca6094782812cc
shristi nepali <shristinepali010@gmail.com>
2026-08-18 12:22:01 +0600

    utils code

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ dns.setServers(["1.1.1.1","8.8.8.8"]);
 
 connectDB()
 .then(()=>{
-        console.log(`server is running at port:${process.env.PORT}`);
-        
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running at port:${process.env.PORT}`);   
     })
-
+})
 .catch((err)=>{
-  console.log("MONGO db connection failed !!!",err);
-  
+    console.log("MONGO db connection failed!!",err);
+    
 })
 
 
